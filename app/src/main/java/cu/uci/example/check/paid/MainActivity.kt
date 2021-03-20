dd2a4a61caac4a8290dbb0066a9b38df636a7765
Daniel Pena Calleja <37417178+dany2709@users.noreply.github.com>
2021-03-20 14:58:39 -0400

    Update MainActivity.kt

--- app/src/main/java/cu/uci/example/check/paid/MainActivity.kt
+++ app/src/main/java/cu/uci/example/check/paid/MainActivity.kt
@@ -1,8 +1,9 @@
 package cu.uci.example.check.paid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import cu.uci.apklischeckpayment.Verify
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,63 @@ class MainActivity : AppCompatActivity() {
 
         val userName = response.second
         val isPaid = response.first
+        val install = response.third
 
-        Toast.makeText(this, "User $userName ${if (isPaid) "paid" else "does not paid"}", Toast.LENGTH_LONG).show()
+        /*
+            action:
+            1 - Install apklis
+            2 - Login in Apklis
+            3 - Purchase app
+        */
+
+        if (install) {
+            // Apklis instalada comprobar compra y login
+
+            if (userName == null) {
+                // Usuario no logueado
+
+                Verify.showAlert(
+                    this,
+                    1,
+                    "com.example.nova.prosalud",
+                    "Usuario no logueado",
+                    "Para poder usar ${getString(R.string.app_name)} debe iniciar sección en Apklis, luego de iniciar sección puede continuar con el uso de ${
+                        getString(R.string.app_name)
+                    }"
+                )
+            } else {
+                // Usuario loguado, comprobar compra
+
+                if (isPaid) {
+                    // Aplicación comprada, su lógica aquí
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Todo esta bien, programe sus siguientes acciones",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    // Aplicación no comprada
+
+                    Verify.showAlert(
+                        this,
+                        2,
+                        "com.example.nova.prosalud",
+                        "Aplicación no comprada",
+                        "Para poder usar ${getString(R.string.app_name)} debe comprar nuestra aplicación, luego de que la compra sea confirmada puede continuar el uso de nuestra aplicación"
+                    )
+                }
+            }
+        } else {
+            // Apklis no instalado
+
+            Verify.showAlert(
+                this,
+                0,
+                "com.example.nova.prosalud",
+                "Apklis no instalada",
+                "Para poder usar ${getString(R.string.app_name)} debe instalar Apklis y luego iniciar sección dentro de esta"
+            )
+        }
     }
 }
